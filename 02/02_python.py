551730920a023e2ad8eb472a285060bdfe78a08d
mglodziak <mikolaj0608@gmail.com>
2019-10-19 00:11:07 +0200

    add -t

--- 02/02_python.py
+++ 02/02_python.py
@@ -8,19 +8,21 @@ def print_help():
       print('-n <number> -> number of sides, default is 4')
       print('-r <number> -> length of each side, default is 50')
       print('-m <number> -> move center, default is 0')
+      print('-t <number> -> turn at start, default is 0')
 
-def draw_figure(length, number, move):
+def draw_figure(length, number, move, turn):
       angle=((int(number)-2)*180)/int(number)
       draw_angle=180-int(angle)
       print(draw_angle)
       x=turtle.Turtle()
+      x.right(int(turn))
       for i in range(1,int(number)+1):
             x.forward(int(length))
             x.right(int(draw_angle))
       input("Press Enter to continue...")
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'm:r:n:h', ['move=', 'length=', 'number', 'help'])
+    opts, args = getopt.getopt(sys.argv[1:], 't:m:r:n:h', ['turn=','move=', 'length=', 'number', 'help'])
 except getopt.GetoptError:
     print_help()
     sys.exit(1)
@@ -28,6 +30,7 @@ except getopt.GetoptError:
 var_n=4
 var_length=50
 var_move=0
+var_turn=0
 
 for opt, arg in opts:
     if opt in ('-h', '--help'):
@@ -39,6 +42,9 @@ for opt, arg in opts:
         
     elif opt in ('-r', '--length'):
         var_length=arg
+
+    elif opt in ('-t', '--turn'):
+        var_turn=arg
         
     elif opt in ('-m', '--move'):
         print('p')
@@ -48,4 +54,4 @@ for opt, arg in opts:
         print_help()
         sys.exit(2)
 
-draw_figure(var_length, var_n, var_move)
+draw_figure(var_length, var_n, var_move, var_turn)
