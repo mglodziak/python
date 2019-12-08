622626b233bc2d0829c89c63eb48235eac3218ae
mglodziak <mikolaj0608@gmail.com>
2019-12-08 23:30:24 +0100

    finish 1 task

--- 08/zadanko.py
+++ 08/zadanko.py
@@ -7,7 +7,8 @@ from random import randrange
 def okrag (x,y,R,ang):
     new_x=R*math.cos(ang*π/180)
     new_y=R*math.sin(ang*π/180)
-    return (x+new_x, y+new_y)
+    
+    return (x+new_x, y+new_y, ang)
 
 
 def generator(x,y,R,krok):
@@ -21,16 +22,36 @@ def rysuj(lista):
     x=turtuś.tracer(False)
     x=turtuś.Turtle()
     turtuś.colormode(255)
+
+    x=turtuś.Turtle()
+    
     for i in lista:
+
+        
         print(i)
         x.penup()
         x.setpos(i[0],i[1])
+        
+        #x.lt(i[2])
+        x.rt(90)
+        x.fd(20)
+        x.lt(90)
+        #x.rt(i[2])
+        
         x.pendown()
         x.fillcolor(randrange(255),randrange(255),randrange(255))
         x.begin_fill()
         x.circle(20)
         x.end_fill()
-    x=turtuś.tracer(True)
+        print(i[2])
+        
+#    x=turtuś.Turtle()
+#    x.rt(90)
+#    x.fd(300)
+#    x.lt(90)
+#    x.circle(300)
+#    x=turtuś.tracer(True)
+
     
 
 x=generator(0,0,300,30)
