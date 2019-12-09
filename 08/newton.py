207236ba66b7f7c3cb5fd1c1baa472597b8f7f53
mglodziak <mikolaj0608@gmail.com>
2019-12-09 13:06:26 +0100

    add newton

---
+++ 08/newton.py
@@ -0,0 +1,13 @@
+from sys import stdin
+from math import factorial
+
+def dwumian(n, k):
+    return factorial(n) // (factorial(k) * factorial(n - k))
+
+
+def dwm_iter(n, k):
+    res=1
+    for i in range (1, k+1):
+        res = res*(n-i +1)/i
+    return res
+        
